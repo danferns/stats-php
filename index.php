cb89d942828fa54b37bf5c043a51bde9651fca61
Daniel <57069381+dannywritescode@users.noreply.github.com>
2019-12-31 19:48:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,11 @@ $statsFile = fopen('../stats.txt','a');
 fwrite($statsFile, date("Y,F,j,G:i A",time()).",".$_SERVER[REQUEST_URI].",".$_SERVER[HTTP_REFERER].",".file_get_contents("https://ipinfo.io/{$_SERVER['REMOTE_ADDR']}/country"));
 fclose($statsFile);
 ?>
-<!DOCTYPE HTML>
-
 <html>
-	<head>
-		<title>Page Title</title>
-	</head>
-  <body>
-    HTML stuff of the page...
-  </body>
+        <head>
+                <title>Hello World!</title>
+        </head>
+        <body>
+                Just wanted to show that HTML content of the page goes in this file too.
+        </body>
 </html>
