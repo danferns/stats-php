587f3dd95c80daaf583c16f33d771114824e77fd
Daniel <57069381+dannywritescode@users.noreply.github.com>
2020-03-09 14:51:09 +0530

    fixed inconsistencies caused by ipinfo
    occasionally, the country label wouldn't end with a newline character. so i've used trim() to remove it when it's there, and added a newline string at the end.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("GMT"); //replce with your time zone https://www.php.net/manual/en/timezones.php
 $statsFile = fopen('stats.txt','a');
-fwrite($statsFile, date("Y,F,j,G:i A",time()).",".$_SERVER[REQUEST_URI].",".$_SERVER[HTTP_REFERER].",".file_get_contents("https://ipinfo.io/{$_SERVER['REMOTE_ADDR']}/country"));
+fwrite($statsFile, date("Y,F,j,G:i A",time()).",".$_SERVER[REQUEST_URI].",".$_SERVER[HTTP_REFERER].",".trim(file_get_contents("https://ipinfo.io/{$_SERVER['REMOTE_ADDR']}/country"))."\n");
 fclose($statsFile);
 ?>
 <html>
