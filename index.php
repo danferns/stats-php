a710a961c296d10ba3b417754d32279e3b57e5ec
Daniel Fernandes <57069381+dannywritescode@users.noreply.github.com>
2020-08-10 22:51:48 +0530

    removed html example
    I realized that a better way to send HTML to the client would be to echo an HTML file. There are probably many other ways this script can be used so I won't keep anything extra.

--- index.php
+++ index.php
@@ -4,11 +4,3 @@ $statsFile = fopen('stats.txt','a');
 fwrite($statsFile, date("Y,F,j,G:i A",time()).",".$_SERVER[REQUEST_URI].",".$_SERVER[HTTP_REFERER].",".trim(file_get_contents("https://ipinfo.io/{$_SERVER['REMOTE_ADDR']}/country"))."\n");
 fclose($statsFile);
 ?>
-<html>
-        <head>
-                <title>Hello World!</title>
-        </head>
-        <body>
-                Just wanted to show that HTML content of the page goes in this file too.
-        </body>
-</html>
